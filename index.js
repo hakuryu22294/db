93f6bd40c05a9c5699dec578966b640b654a8340
QH <quanghack12b@gmail.com>
2024-01-30 22:55:21 +0700

    auth

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import jsonServer from 'json-server'
+import jsonServer from "json-server";
+import auth from "json-server-auth";
 
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const auth = auth.create();
+const router = jsonServer.router("db.json");
 const middleware = jsonServer.defaults();
 const PORT = 8080;
 server.use(middleware);
 server.use(router);
-server.listen(PORT)
+server.use(auth);
+server.listen(PORT);
