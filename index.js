380244f64633d7d90d1daefc396c3c58c0a8b146
QH <quanghack12b@gmail.com>
2024-01-31 18:41:05 +0700

    cx

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
+const cors = require("cors");
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
@@ -8,6 +9,7 @@ const router = jsonServer.router("db.json");
 app.db = router.db;
 
 // You must apply the auth middleware before the router
+app.use(cors());
 app.use(auth);
 app.use(router);
 app.listen(3000, () => console.log("server start"));
